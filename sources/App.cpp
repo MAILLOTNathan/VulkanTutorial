eb25f0ffd79437c88562d4c20284f5905bdcd839
Nathan Maillot <nathan.maillot@epitech.eu>
2026-01-13 00:07:15 +0900

    fix: stb image include

--- sources/App.cpp
+++ sources/App.cpp
@@ -8,7 +8,7 @@
 #include "App.hpp"
 
 #define STB_IMAGE_IMPLEMENTATION
-#include <stb/stb_image.h>
+#include "stb_image.h"
 
 #define TINYOBJLOADER_IMPLEMENTATION
 #include <tinyobjloader/tiny_obj_loader.h>
