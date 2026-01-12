d432e68a9003861ceac106c54e86dc2daf27fab4
Nathan Maillot <nathan.maillot@epitech.eu>
2026-01-13 00:14:32 +0900

    fix: TOL include name

--- sources/App.cpp
+++ sources/App.cpp
@@ -11,7 +11,7 @@
 #include "stb_image.h"
 
 #define TINYOBJLOADER_IMPLEMENTATION
-#include <tinyobjloader/tiny_obj_loader.h>
+#include "tiny_obj_loader.h"
 
 static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, VkDebugUtilsMessageTypeFlagsEXT messageType, const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, void* pUserData)
 {
